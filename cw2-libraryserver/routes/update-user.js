37bdf04973f795485e8b1ad575a194fb2174afe4
Armando Castany Marconi <castanymarconiaj1@cardiff.ac.uk>
2019-01-20 11:55:11 +0000

    Add: Loans layout (.pug)
    Missing: JS

--- cw2-libraryserver/routes/update-user.js
+++ cw2-libraryserver/routes/update-user.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
-  res.render('update-user', {title: 'Update User'});
+  res.render('update-user', {title: 'Update Users'});
 });
 // router.get("../", function(req, res, next) {
 //   res.render('navigation', {title: 'navigation'});
